cc8e83497b508e3e0e83aaf445294b058baa2ca8
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-25 05:44:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -36,23 +36,7 @@ def nyc_pigeon_organizer(data)
   }
 
 
-  data[:color].each { |pColor,pName| #pName is an array
-    newHash.each { |hName,hValue| #hName are hashes from the pigeons names, and Hvalue are their atributes
-      if pName.include?(hName) #working until here, should I loop inside it?
-        newHash[hName][:color].push(pColor.to_s) #My problem is, it only adds the last color
-        hValue.each { |key,thisshit|thisshit.push(pColor)}
-      end
-    }
-  }
-
-
-  data[:lives].each { |pLives,pName| #pName is an array
-  newHash.each { |hName,hValue| #hName are hashes from the pigeons names, and Hvalue are their atributes
-    if pName.include?(hName) #working until here, should I loop inside it?
-      newHash[hName][:lives].push(pLives) #My problem is, it only adds the last color
-        hValue.each { |key,thisshit|thisshit.push(pColor)}
-    end
-  }
+  
    
   }
   puts newHash
