e1e5bea8d28f62c4830324dbd25a12008548626e
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-25 05:41:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -40,9 +40,7 @@ def nyc_pigeon_organizer(data)
     newHash.each { |hName,hValue| #hName are hashes from the pigeons names, and Hvalue are their atributes
       if pName.include?(hName) #working until here, should I loop inside it?
         newHash[hName][:color].push(pColor.to_s) #My problem is, it only adds the last color
-        hValue.each { |key,thisshit|
-        thisshit.push(pColor)
-        }
+        hValue.each { |key,thisshit|thisshit.push(pColor)}
       end
     }
   }
